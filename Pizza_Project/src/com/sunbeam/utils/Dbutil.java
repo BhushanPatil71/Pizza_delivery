f667ddf0dc7ce7994558bfa85678eac095b38ac7
Bhushan Patil <bnpatil.71@gmail.com>
2024-11-05 15:00:55 +0530

    Pizza application bare bones structure

---
+++ Pizza_Project/src/com/sunbeam/utils/Dbutil.java
@@ -0,0 +1,15 @@
+package com.sunbeam.utils;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class Dbutil {
+	private static final String URL = "jdbc:mysql://localhost:3306/sunbeam_hackathon";
+	private static final String USERNAME = "D3_87042_Bhushan";
+	private static final String PASSWORD = "manager";
+
+	public static Connection getConnection() throws SQLException {
+		return DriverManager.getConnection(URL, USERNAME, PASSWORD);
+	}
+}
